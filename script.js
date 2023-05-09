093388fbf53ec9d18de0df220afe15d954a75c1a
H3nr1qu3x <104533111+H3NR1QU3x@users.noreply.github.com>
2023-05-09 10:58:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var slider = document.getElementById("mySlider");
 var output = document.getElementById("sliderValue");
 var image = document.getElementById("sliderImage");
-var imagePath = "img/icons/"; // Caminho para as imagens
+var imagePath = "img/icons/";
+var imagePathTwo = "img/avatars/";
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
   updateImage(this.value);
 }
@@ -15,64 +16,88 @@ function updateImage(value) {
   image.src = fullPath;
 }
 
-function mostrarTexto() {
-  var texto = document.getElementById("texto").value;
-  if(texto == ""){
+function showText() {
+  var user = document.getElementById("Username").value;
+  if (user == "") {
     alert("Write a name please!");
   }
-  else if(texto.includes(" ")){
+  else if (user.includes(" ")) {
     alert("No spaces!");
   }
-  else{
-    document.getElementById("texto-digitado").innerHTML = texto;
+  else {
+    document.getElementById("text-digitado").innerHTML = user;
   }
-  
+
 }
 
-function mostrarXp() {
-  
+function showXp() {
+
 
   var numXp = document.getElementById("numXp").value;
-  if(numXp == ""){
-    alert("Write a name please!");
-  }
-  else if(numXp.includes(" ")){
-    alert("No spaces!");
-  }
-  else{
-    document.getElementById("numXp-digitado").innerHTML = numXp;
-  }
-  
-}
-
-function mostrarWins() {
-  var numWins = document.getElementById("numWins").value;
-  if(numWins == ""){
+  if (numXp == "") {
     alert("Write a number please!");
   }
-  else if(numWins.includes(" ")){
+  else if (numXp.includes(" ")) {
     alert("No spaces!");
   }
-  else{
-    document.getElementById("numWins-digitado").innerHTML = numWins;
+  else {
+    document.getElementById("numXp-digitado").innerHTML = numXp;
   }
-  
+
 }
 
-document.getElementById("btnScreenshot").addEventListener("click", function() {
-  // Captura a div
-  html2canvas(document.querySelector("#minha-div")).then(function(canvas) {
-    // Cria uma imagem para download
+function showWins() {
+  var numWins = document.getElementById("numWins").value;
+  if (numWins == "") {
+    alert("Write a number please!");
+  }
+  else if (numWins.includes(" ")) {
+    alert("No spaces!");
+  }
+  else {
+    document.getElementById("numWins-digitado").innerHTML = numWins;
+  }
+
+}
+
+document.getElementById("btnScreenshot").addEventListener("click", function () {
+  html2canvas(document.querySelector("#screnshotdiv")).then(function (canvas) {
     var img = canvas.toDataURL("image/png");
-    // Cria um link para download
     var link = document.createElement('a');
-    link.download = 'screenshot.png';
+    link.download = 'signature.png';
     link.href = img;
-    // Adiciona o link ao documento
     document.body.appendChild(link);
-    // Clica no link para iniciar o download
     link.click();
-    // Remove o link do documento
     document.body.removeChild(link);
   });
 });
+
+function switchTag() {
+  document.getElementById("avatars").src="img/avatars/tag.png";
+}
+
+function switchChief() {
+  document.getElementById("avatars").src="img/avatars/chief.png";
+}
+
+function switchEspresso() {
+  document.getElementById("avatars").src="img/avatars/espresso.png";
+}
+
+function switchJaz() {
+  document.getElementById("avatars").src="img/avatars/jaz.png";
+}
+
+function switchHale() {
+  document.getElementById("avatars").src="img/avatars/hale.png";
+}
+
+function showAvatar(input) {
+  if (input.files && input.files[0]) {
+    var who = new FileReader();
+    who.onload = function(e) {
+      document.getElementById('avatars').src = e.target.result;
+    }
+    who.readAsDataURL(input.files[0]);
+  }
+}
